47a194e07a9aa14dad05dcab42557a92c0a860d2
Marvin Schlegel <5002075+cybniv@users.noreply.github.com>
2023-06-14 20:48:39 +0200

    refactor: use dataclass_transform from typing_extensions (#236)
    Upsteam strawberrys switched to using `dataclass_transform` from
    typing_extensions (>=4.1.0) for their latest release [1]
    
    So, analogue to [2], let's make the switch. For strawberry's PR, see [3]
    
    [1] https://github.com/strawberry-graphql/strawberry/releases/tag/0.185.0
    [2]
    https://github.com/strawberry-graphql/strawberry/commit/e055b127dd7391ccafdb6d96fe9b7463fa655451
    [3] https://github.com/strawberry-graphql/strawberry/pull/2227

--- strawberry_django_plus/ordering.py
+++ strawberry_django_plus/ordering.py
@@ -4,9 +4,9 @@ import strawberry
 from django.db.models.base import Model
 from strawberry import UNSET
 from strawberry.field import StrawberryField
-from strawberry.utils.typing import __dataclass_transform__
 from strawberry_django.fields.field import field as _field
 from strawberry_django.ordering import Ordering
+from typing_extensions import dataclass_transform
 
 from strawberry_django_plus.utils.typing import is_auto
 
@@ -16,9 +16,9 @@ from .relay import connection, node
 _T = TypeVar("_T")
 
 
-@__dataclass_transform__(
+@dataclass_transform(
     order_default=True,
-    field_descriptors=(
+    field_specifiers=(
         StrawberryField,
         _field,
         node,
